6b248b34dbd8c780acbba0bf6799956520d3bcf8
youmi301 <youmi301@gmail.com>
2019-01-03 14:27:06 +0900

    Inital commmit

---
+++ src/main/java/com/channelg/myrestapi_test/MyrestapiTestApplication.java
@@ -0,0 +1,14 @@
+package com.channelg.myrestapi_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrestapiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrestapiTestApplication.class, args);
+    }
+
+}
+
