38df64f3e19f038f45132216a3d50577dcc55a85
Rosa <rosayoumi@gmail.com>
2019-01-04 11:30:14 +0900

    various bad request test

--- src/main/java/com/channelg/myrestapi_test/events/EventController.java
+++ src/main/java/com/channelg/myrestapi_test/events/EventController.java
@@ -4,10 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -18,14 +20,26 @@ public class EventController {
 
     private final EventReposirory eventReposirory;
     private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
 
-    public EventController(EventReposirory eventReposirory, ModelMapper modelMapper) {
+    public EventController(EventReposirory eventReposirory, ModelMapper modelMapper, EventValidator eventValidator) {
         this.eventReposirory = eventReposirory;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+
+        if (errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        eventValidator.validate(eventDto, errors);
+        if (errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
         Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventReposirory.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- src/test/java/com/channelg/myrestapi_test/events/EventControllerTests.java
+++ src/test/java/com/channelg/myrestapi_test/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package com.channelg.myrestapi_test.events;
 
+import com.channelg.myrestapi_test.common.TestDescription;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -41,11 +42,44 @@ public class EventControllerTests {
     //EventReposirory eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("Rest API TEST ....")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,20))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,25))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,25))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+        //Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+            .andDo(print())
+            //.andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            //.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+            //.andExpect(jsonPath("id").value(Matchers.not(100)))
+            //.andExpect(jsonPath("free").value(Matchers.not(true)))
+            .andExpect(jsonPath("eventStatus").value(EventStatus.DREFT.name()))
+        ;
+    }
+
+    @Test
+    @TestDescription("입력받을 수 없는 값을 사용한 경우에 오류를 발생하는 테스트")
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
-                .description("Rst API TEST ....")
+                .description("Rest API TEST ....")
                 .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
                 .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,20))
                 .beginEventDateTime(LocalDateTime.of(2018,11,25,14,25))
@@ -58,20 +92,46 @@ public class EventControllerTests {
                 .offline(false)
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
-        //Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
             .andDo(print())
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("id").exists())
-            .andExpect(header().exists(HttpHeaders.LOCATION))
-            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-            .andExpect(jsonPath("id").value(Matchers.not(100)))
-            .andExpect(jsonPath("free").value(Matchers.not(true)))
-            .andExpect(jsonPath("eventStatus").value(EventStatus.DREFT.name()))
-        ;
+            .andExpect(status().isBadRequest())
+            ;
+    }
+
+    @Test
+    @TestDescription("입력값이 비어있는 경우에 오류가 발생하는 테스트")
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @TestDescription("입력값이 잘못되어 있는 경우에 오류가 발생하는 테스트")
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("Rest API TEST ....")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,20))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,25))
+                .endEventDateTime(LocalDateTime.of(2018,11,24,14,25))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
     }
 }
