1f02248b8dd4134c1b06695bb39d446caf9cc617
Rosa <rosayoumi@gmail.com>
2019-01-04 12:53:43 +0900

    body for bad request

--- src/main/java/com/channelg/myrestapi_test/events/EventValidator.java
+++ src/main/java/com/channelg/myrestapi_test/events/EventValidator.java
@@ -10,8 +10,9 @@ import java.time.LocalDateTime;
 public class EventValidator {
     public void validate(EventDto eventDto, Errors errors){
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong.");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong.");
+            //errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong.");
+            //errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong.");
+            errors.reject("wrongPrices", "Value of Price is wrong.");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
